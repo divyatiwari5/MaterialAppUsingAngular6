3f1ce5f4a4276ea166d9ca5c3a0222f626606b15
divya <tiwari.divya50@gmail.com>
2018-05-18 11:57:29 +0530

    Added autocomplete feature and autoactive first element feature in Autocomplete view

--- src/app/mat-comp/mat-comp.component.ts
+++ src/app/mat-comp/mat-comp.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormsModule} from '@angular/forms';
+import {FormControl} from '@angular/forms';
+import { Observable} from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-mat-comp',
@@ -13,10 +16,22 @@ export class MatCompComponent implements OnInit {
   Options = [
      'Option1',
       'Option2',
-      'Option3'
+      'Option3',
+    'number',
+    'mammal'
   ];
-  constructor() { }
+
+
+  filteredOptions: Observable<string[]>;
 
   ngOnInit() {
+    this.filteredOptions = this.form_control.valueChanges
+      .pipe(startWith(''),
+       map(val => this.filter(val)));
+  }
+
+  filter(val: string): string[] {
+    return this.Options.filter(option => option.toLowerCase().includes(val.toLowerCase()));
+
   }
 }
