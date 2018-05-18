1366788739c966a927e34d256de19adc6fc0a6e7
divya <tiwari.divya50@gmail.com>
2018-05-18 15:30:54 +0530

    Added navigation bar Layout with a left position and end position sidenav and explicit content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { MatInputModule, MatAutocompleteModule, MatFormFieldModule } from '@angular/material';
+import { MatInputModule, MatAutocompleteModule, MatFormFieldModule, MatCheckboxModule, MatSidenavModule } from '@angular/material';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatCompComponent } from './mat-comp/mat-comp.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    MatCompComponent
+    MatCompComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule, FormsModule, BrowserAnimationsModule,
+    BrowserModule, FormsModule, BrowserAnimationsModule, MatCheckboxModule, MatSidenavModule,
     ReactiveFormsModule, MatAutocompleteModule,  MatInputModule, MatFormFieldModule],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/mat-comp/mat-comp.component.ts
+++ src/app/mat-comp/mat-comp.component.ts
@@ -1,37 +1,86 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl} from '@angular/forms';
+import {FormBuilder, FormControl, FormGroup} from '@angular/forms';
 import { Observable} from 'rxjs';
 import {map, startWith} from 'rxjs/operators';
+import {group} from '@angular/animations';
 
+
+export interface GroupOption {
+  letter: string;
+  g_option: string[];
+}
 
 @Component({
   selector: 'app-mat-comp',
   templateUrl: './mat-comp.component.html',
   styleUrls: ['./mat-comp.component.css']
 })
+
+
 export class MatCompComponent implements OnInit {
 
   form_control: FormControl = new FormControl();
+  formgroup: FormGroup = this.formbuild.group({
+    groupoption: '',
+  });
 
+  GroupOptions: GroupOption[] = [
+    {
+      letter: 'A',
+      g_option: ['Anamika', 'Amrita', 'Amaira', 'Aaeysha']
+    },
+    {
+      letter: 'B',
+      g_option: ['Basmandi', 'Besharam', 'Babitaji :D']
+    },
+    {
+      letter: 'D',
+      g_option: ['Divya', 'Divyajyoti']
+    },
+    {
+      letter: 'H',
+      g_option: ['Himanshu', 'Divyanshu']
+    }
+  ];
   Options = [
-     'Option1',
-      'Option2',
-      'Option3',
+    'Option1',
+    'Option2',
+    'Option3',
     'number',
     'mammal'
   ];
 
-
+  groupFilterOptions: Observable<GroupOption[]>;
   filteredOptions: Observable<string[]>;
+
+  constructor(private formbuild: FormBuilder) {
+  }
 
   ngOnInit() {
     this.filteredOptions = this.form_control.valueChanges
       .pipe(startWith(''),
-       map(val => this.filter(val)));
+        map(val => this.filter(val)));
+
+    this.groupFilterOptions = this.formgroup.get('groupoption')!.valueChanges
+      .pipe(startWith(''), map(value => this.GroupFilterFunction(value)));
   }
 
   filter(val: string): string[] {
     return this.Options.filter(option => option.toLowerCase().includes(val.toLowerCase()));
 
   }
+
+  GroupFilterFunction(val: string): GroupOption[] {
+    if (val) {
+      return this.GroupOptions
+        .map(group => ({letter: group.letter, g_option: this._filter(group.g_option, val)}))
+        .filter(group => group.g_option.length > 0);
+    }
+    return this.GroupOptions;
+  }
+  private _filter(opt: string[], val: string) {
+    const filterValue = val.toLowerCase();
+    return opt.filter(item => item.toLowerCase().startsWith(filterValue));
+  }
 }
+
